86c98b4a9ec283c53633d31a962dfbe00a6b2aff
Aaron Berry <aaron.abm@gmail.com>
2019-02-27 22:31:00 +1100

    100% test coverage

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,237 +4,197 @@ const helpers = require("yeoman-test");
 const assert = require("yeoman-assert");
 const fs = require("fs");
 
-describe("Include meta is true", () => {
-  const roleName = "test";
-  const authorName = "John smith";
-  const description = "Role description";
-  const metaLicense = "MIT";
-  const includeMeta = true;
-  const metaCompany = "Company co";
+const buildPkgJson = params => {
+  let template = {
+    name: params.roleName,
+    version: "1.0.0",
+    main: "index.js",
+    repository: "git@github.com:Aaron-K-T-Berry/generator-ansible.git",
+    description: params.description,
+    author: params.authorName,
+    license: params.metaLicense,
+    dependencies: {},
+    scripts: {
+      venv: "virtualenv venv",
+      "venv-activate": "source venv/bin/activate",
+      requirements: "pip install -r requirements.txt"
+    }
+  };
+
+  if (params.includeMolecule) {
+    template = {
+      ...template,
+      ...{
+        check: "molecule check",
+        converge: "molecule converge",
+        create: "molecule create",
+        dependency: "molecule dependency",
+        destroy: "molecule destroy",
+        idempotence: "molecule idempotence",
+        lint: "molecule lint",
+        list: "molecule list",
+        login: "molecule login",
+        matrix: "molecule matrix",
+        prepare: "molecule prepare",
+        "side-effect": "molecule side-effect",
+        syntax: "molecule syntax",
+        test: "molecule test",
+        verify: "molecule verify"
+      }
+    };
+  }
+  return template;
+};
+
+const defaultPrompts = {
+  roleName: "test",
+  authorName: "John smith",
+  description: "Role description",
+  metaLicense: "MIT",
+  includeMolecule: false,
+  includeMeta: false,
+  metaCompany: "Company co."
+};
+
+describe("Defaults included", () => {
+  const prompts = { ...defaultPrompts };
 
   beforeEach(() => {
-    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
-      roleName,
-      authorName,
-      description,
-      metaLicense,
-      includeMeta,
-      metaCompany
-    });
+    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts(prompts);
   });
 
   it("Copied defaults/main.yml", () => {
-    assert.file(`${roleName}/defaults/main.yml`);
+    assert.file(`${prompts.roleName}/defaults/main.yml`);
   });
 
   it("Copied handlers/main.yml", () => {
-    assert.file(`${roleName}/handlers/main.yml`);
+    assert.file(`${prompts.roleName}/handlers/main.yml`);
   });
 
   it("Copied meta/main.yml", () => {
-    assert.file(`${roleName}/meta/main.yml`);
-  });
-
-  it("Contents meta/main.yml", () => {
-    assert.fileContent(`${roleName}/meta/main.yml`, `author: ${authorName}`);
-    assert.fileContent(`${roleName}/meta/main.yml`, `description: ${description}`);
-    assert.fileContent(`${roleName}/meta/main.yml`, `company: ${metaCompany}`);
-    assert.fileContent(`${roleName}/meta/main.yml`, `license: ${metaLicense}`);
+    assert.noFile(`${prompts.roleName}/meta/main.yml`);
   });
 
   it("Copied molecule/default/tests files", () => {
-    assert.file(`${roleName}/molecule/default/tests/test_default.py`);
-    assert.file(`${roleName}/molecule/default/tests/test_default.pyc`);
+    assert.noFile(`${prompts.roleName}/molecule/default/tests/test_default.py`);
+    assert.noFile(`${prompts.roleName}/molecule/default/tests/test_default.pyc`);
   });
 
   it("Copied molecule/default files", () => {
-    assert.file(`${roleName}/molecule/default/Dockerfile.j2`);
-    assert.file(`${roleName}/molecule/default/INSTALL.rst`);
-    assert.file(`${roleName}/molecule/default/molecule.yml`);
-    assert.file(`${roleName}/molecule/default/playbook.yml`);
-  });
-
-  it("Contents of molecule/default/playbook.yml", () => {
-    assert.fileContent(`${roleName}/molecule/default/playbook.yml`, `- role: ${roleName}`);
+    assert.noFile(`${prompts.roleName}/molecule/default/Dockerfile.j2`);
+    assert.noFile(`${prompts.roleName}/molecule/default/INSTALL.rst`);
+    assert.noFile(`${prompts.roleName}/molecule/default/molecule.yml`);
+    assert.noFile(`${prompts.roleName}/molecule/default/playbook.yml`);
   });
 
   it("Copied tasks/main.yml", () => {
-    assert.file(`${roleName}/tasks/main.yml`);
+    assert.file(`${prompts.roleName}/tasks/main.yml`);
   });
 
   it("Copied vars/main.yml", () => {
-    assert.file(`${roleName}/vars/main.yml`);
+    assert.file(`${prompts.roleName}/vars/main.yml`);
   });
 
   it("Copied .yamllint", () => {
-    assert.file(`${roleName}/.yamllint`);
+    assert.file(`${prompts.roleName}/.yamllint`);
   });
 
   it("Copied README.md", () => {
-    assert.file(`${roleName}/README.md`);
+    assert.file(`${prompts.roleName}/README.md`);
   });
 
   it("Contents of README.md", () => {
-    assert.fileContent(`${roleName}/README.md`, roleName);
-    assert.fileContent(`${roleName}/README.md`, `Author: ${authorName}`);
+    assert.fileContent(`${prompts.roleName}/README.md`, prompts.roleName);
+    assert.fileContent(`${prompts.roleName}/README.md`, `Author: ${prompts.authorName}`);
   });
 
   it("Copied requirements.txt", () => {
-    assert.file(`${roleName}/requirements.txt`);
+    assert.file(`${prompts.roleName}/requirements.txt`);
   });
 
   it("Copied run-test.sh", () => {
-    assert.file(`${roleName}/requirements.txt`);
+    assert.file(`${prompts.roleName}/requirements.txt`);
   });
 
   it("Copied package.json", () => {
-    assert.file(`${roleName}/package.json`);
+    assert.file(`${prompts.roleName}/package.json`);
   });
 
-  it("content package.json", () => {
-    assert.JSONFileContent(`${roleName}/package.json`, {
-      name: roleName,
-      version: "1.0.0",
-      main: "index.js",
-      repository: "git@github.com:Aaron-K-T-Berry/generator-ansible.git",
-      description: description,
-      author: authorName,
-      license: metaLicense,
-      dependencies: {},
-      scripts: {
-        venv: "virtualenv venv",
-        "venv-activate": "source venv/bin/activate",
-        requirements: "pip install -r requirements.txt",
-        check: "molecule check",
-        converge: "molecule converge",
-        create: "molecule create",
-        dependency: "molecule dependency",
-        destroy: "molecule destroy",
-        idempotence: "molecule idempotence",
-        lint: "molecule lint",
-        list: "molecule list",
-        login: "molecule login",
-        matrix: "molecule matrix",
-        prepare: "molecule prepare",
-        "side-effect": "molecule side-effect",
-        syntax: "molecule syntax",
-        test: "molecule test",
-        verify: "molecule verify"
-      }
-    });
+  it("Content package.json", () => {
+    assert.JSONFileContent(`${prompts.roleName}/package.json`, buildPkgJson(prompts));
   });
 });
 
-describe("Include meta is false", () => {
+describe("Molecule included", () => {
+  const prompts = { ...defaultPrompts, includeMolecule: true };
+
+  beforeEach(() => {
+    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts(prompts);
+  });
+
+  it("Copied molecule/default/tests files", () => {
+    assert.file(`${prompts.roleName}/molecule/default/tests/test_default.py`);
+    assert.file(`${prompts.roleName}/molecule/default/tests/test_default.pyc`);
+  });
+
+  it("Copied molecule/default files", () => {
+    assert.file(`${prompts.roleName}/molecule/default/Dockerfile.j2`);
+    assert.file(`${prompts.roleName}/molecule/default/INSTALL.rst`);
+    assert.file(`${prompts.roleName}/molecule/default/molecule.yml`);
+    assert.file(`${prompts.roleName}/molecule/default/playbook.yml`);
+  });
+
+  it("Contents of molecule/default/playbook.yml", () => {
+    assert.fileContent(
+      `${prompts.roleName}/molecule/default/playbook.yml`,
+      `- role: ${prompts.roleName}`
+    );
+  });
+
+  it("Content package.json", () => {
+    assert.JSONFileContent(`${prompts.roleName}/package.json`, buildPkgJson(prompts));
+  });
+});
+
+describe("CircleCi included", () => {
+  const prompts = { ...defaultPrompts, includeMolecule: true, includeCircleCi: true };
+
+  beforeEach(() => {
+    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts(prompts);
+  });
+
+  it("Copied .circleci/config.yml", () => {
+    assert.file(`${prompts.roleName}/.circleci/config.yml`);
+  })
+});
+
+describe("Meta included", () => {
+  const prompts = { ...defaultPrompts, ...{ includeMeta: true, metaCompany: "Company co." } };
+
   const roleName = "test";
   const authorName = "John smith";
   const description = "Role description";
   const metaLicense = "MIT";
-  const includeMeta = false;
-  const metaCompany = "Company co";
+  const includeMolecule = false;
+  const includeMeta = true;
+  const metaCompany = "Company co.";
 
   beforeEach(() => {
-    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
-      roleName,
-      authorName,
-      description,
-      metaLicense,
-      includeMeta,
-      metaCompany
-    });
+    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts(prompts);
   });
 
-  it("Copied defaults/main.yml", () => {
-    assert.file(`${roleName}/defaults/main.yml`);
+  it("Copied meta/main.yml", () => {
+    assert.file(`${prompts.roleName}/meta/main.yml`);
   });
 
-  it("Copied handlers/main.yml", () => {
-    assert.file(`${roleName}/handlers/main.yml`);
+  it("Contents meta/main.yml", () => {
+    assert.fileContent(`${prompts.roleName}/meta/main.yml`, `author: ${authorName}`);
+    assert.fileContent(`${prompts.roleName}/meta/main.yml`, `description: ${description}`);
+    assert.fileContent(`${prompts.roleName}/meta/main.yml`, `company: ${metaCompany}`);
+    assert.fileContent(`${prompts.roleName}/meta/main.yml`, `license: ${metaLicense}`);
   });
 
-  it("Did not copy meta/main.yml", () => {
-    assert.noFile(`${roleName}/meta/main.yml`);
-  });
-
-  it("Copied molecule/default/tests files", () => {
-    assert.file(`${roleName}/molecule/default/tests/test_default.py`);
-    assert.file(`${roleName}/molecule/default/tests/test_default.pyc`);
-  });
-
-  it("Copied molecule/default files", () => {
-    assert.file(`${roleName}/molecule/default/Dockerfile.j2`);
-    assert.file(`${roleName}/molecule/default/INSTALL.rst`);
-    assert.file(`${roleName}/molecule/default/molecule.yml`);
-    assert.file(`${roleName}/molecule/default/playbook.yml`);
-  });
-
-  it("Contents of molecule/default/playbook.yml", () => {
-    assert.fileContent(`${roleName}/molecule/default/playbook.yml`, `- role: ${roleName}`);
-  });
-
-  it("Copied tasks/main.yml", () => {
-    assert.file(`${roleName}/tasks/main.yml`);
-  });
-
-  it("Copied vars/main.yml", () => {
-    assert.file(`${roleName}/vars/main.yml`);
-  });
-
-  it("Copied .yamllint", () => {
-    assert.file(`${roleName}/.yamllint`);
-  });
-
-  it("Copied README.md", () => {
-    assert.file(`${roleName}/README.md`);
-  });
-
-  it("Contents of README.md", () => {
-    assert.fileContent(`${roleName}/README.md`, roleName);
-    assert.fileContent(`${roleName}/README.md`, `Author: ${authorName}`);
-  });
-
-  it("Copied requirements.txt", () => {
-    assert.file(`${roleName}/requirements.txt`);
-  });
-
-  it("Copied run-test.sh", () => {
-    assert.file(`${roleName}/requirements.txt`);
-  });
-
-  it("Copied package.json", () => {
-    assert.file(`${roleName}/package.json`);
-  });
-
-  it("content package.json", () => {
-    assert.JSONFileContent(`${roleName}/package.json`, {
-      name: roleName,
-      version: "1.0.0",
-      main: "index.js",
-      repository: "git@github.com:Aaron-K-T-Berry/generator-ansible.git",
-      description: description,
-      author: authorName,
-      license: metaLicense,
-      dependencies: {},
-      scripts: {
-        venv: "virtualenv venv",
-        "venv-activate": "source venv/bin/activate",
-        requirements: "pip install -r requirements.txt",
-        check: "molecule check",
-        converge: "molecule converge",
-        create: "molecule create",
-        dependency: "molecule dependency",
-        destroy: "molecule destroy",
-        idempotence: "molecule idempotence",
-        lint: "molecule lint",
-        list: "molecule list",
-        login: "molecule login",
-        matrix: "molecule matrix",
-        prepare: "molecule prepare",
-        "side-effect": "molecule side-effect",
-        syntax: "molecule syntax",
-        test: "molecule test",
-        verify: "molecule verify"
-      }
-    });
+  it("Content package.json", () => {
+    assert.JSONFileContent(`${prompts.roleName}/package.json`, buildPkgJson(prompts));
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -6,7 +6,8 @@ const mkdirp = require("mkdirp");
 const path = require("path");
 const _ = require("lodash");
 const exec = require("child_process").exec;
-const gitConfig = require("git-config");
+// TODO [GIT] GET GIT CONFIGS
+// const gitConfig = require("git-config");
 
 const licenses = [
   { name: "Apache 2.0", value: "Apache-2.0" },
@@ -32,9 +33,9 @@ const getLicenseValue = name => {
 
 module.exports = class extends Generator {
   prompting() {
-    // Have Yeoman greet the user.
+    
     this.log(yosay(`Welcome to the groundbreaking ${chalk.red("generator-ansible")} generator!`));
-
+    
     const prompts = [
       {
         type: "text",
@@ -59,6 +60,21 @@ module.exports = class extends Generator {
       },
       {
         type: "confirm",
+        name: "includeMolecule",
+        message: "Would you like to include the molecule testing boilerplate?",
+        default: false
+      },
+      {
+        when: response => {
+          return response.includeMolecule;
+        },
+        type: "text",
+        name: "includeCircleCi",
+        message: "Would you like to include a basic circle ci config for molecule?",
+        default: false
+      },
+      {
+        type: "confirm",
         name: "includeMeta",
         message: "Would you like to include the meta folder?",
         default: false
@@ -75,43 +91,25 @@ module.exports = class extends Generator {
 
     return this.prompt(prompts).then(props => {
       this.props = props;
-
       // Formatting some props
       // this.props.roleName = _.kebabCase(this.props.roleName);
     });
   }
 
-  default() {    
+  default() {
     // Folder root
     const roleRoot = `./${this.props.roleName}`;
     this.props.roleRoot = roleRoot;
     mkdirp.sync(roleRoot);
-    this.fs.copy(
-      // Non dotfiles
-      this.templatePath("./**"),
-      roleRoot
-    );
-    this.fs.copy(
-      // Dotfiles
-      this.templatePath("./.*"),
-      roleRoot
-    );
-    const readMeTemplate = _.template(this.fs.read(this.templatePath("README.md")));
-    this.fs.write(
-      path.join(roleRoot, "README.md"),
-      readMeTemplate({
-        roleName: this.props.roleName,
-        authorName: this.props.authorName
-      })
-    );
-    const runScriptTemplate = _.template(this.fs.read(this.templatePath("run-test.sh")));
-    this.fs.write(
-      path.join(roleRoot, "run-test.sh"),
-      runScriptTemplate({
-        roleName: this.props.roleName
-      })
-    );
+    this.fs.copy(this.templatePath("root-files"), roleRoot);
+    this.fs.copy(this.templatePath("root-files/.**"), roleRoot);
 
+    // CIRCLECI
+    if(this.props.includeCircleCi){
+      mkdirp.sync(path.join(roleRoot, ".circleci"));
+      this.fs.copy(this.templatePath(".circleci"), path.join(roleRoot, ".circleci"));
+    }
+    
     // DEFAULTS
     mkdirp.sync(path.join(roleRoot, "defaults"));
     this.fs.copy(this.templatePath("defaults"), path.join(roleRoot, "defaults"));
@@ -120,18 +118,6 @@ module.exports = class extends Generator {
     mkdirp.sync(path.join(roleRoot, "handlers"));
     this.fs.copy(this.templatePath("handlers"), path.join(roleRoot, "handlers"));
 
-    // MOLECULE
-    mkdirp.sync(path.join(roleRoot, "molecule/default/tests"));
-    this.fs.copy(this.templatePath("molecule"), path.join(roleRoot, "molecule"));
-    const playbookTemplate = _.template(
-      this.fs.read(this.templatePath("molecule/default/playbook.yml"))
-    );
-    this.fs.write(
-      path.join(roleRoot, "molecule/default/playbook.yml"),
-      playbookTemplate({
-        roleName: this.props.roleName
-      })
-    );
     // TASKS
     mkdirp.sync(path.join(roleRoot, "tasks"));
     this.fs.copy(this.templatePath("tasks"), path.join(roleRoot, "tasks"));
@@ -142,12 +128,45 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    if (this.props.includeMeta) {
-      const metaTemplate = _.template(this.fs.read(this.templatePath('meta/main.yml')))
+    const roleRoot = `./${this.props.roleName}`;
+
+    // MOLECULE
+    if (this.props.includeMolecule) {
+      mkdirp.sync(path.join(roleRoot, "molecule/default/tests"));
+      this.fs.copy(this.templatePath("molecule"), path.join(roleRoot, "molecule"));
+      const playbookTemplate = _.template(
+        this.fs.read(this.templatePath("molecule/default/playbook.yml"))
+      );
       this.fs.write(
-        path.join(this.props.roleName, "meta/main.yml"),
+        path.join(roleRoot, "molecule/default/playbook.yml"),
+        playbookTemplate({
+          roleName: this.props.roleName
+        })
+      );
+    }
+
+    const readMeTemplate = _.template(this.fs.read(this.templatePath("root-files/README.md")));
+    this.fs.write(
+      path.join(roleRoot, "README.md"),
+      readMeTemplate({
+        roleName: this.props.roleName,
+        authorName: this.props.authorName
+      })
+    );
+    const runScriptTemplate = _.template(this.fs.read(this.templatePath("root-files/run-test.sh")));
+    this.fs.write(
+      path.join(roleRoot, "run-test.sh"),
+      runScriptTemplate({
+        roleName: this.props.roleName
+      })
+    );
+
+    // META
+    if (this.props.includeMeta) {
+      const metaTemplate = _.template(this.fs.read(this.templatePath("meta/main.yml")));
+      this.fs.write(
+        path.join(roleRoot, "meta/main.yml"),
         metaTemplate({
-          authorName: this.props,
           authorName: this.props.authorName,
           description: this.props.description,
           metaCompany: this.props.metaCompany,
@@ -155,12 +174,13 @@ module.exports = class extends Generator {
         })
       );
     }
-    
-    const pkgTemplate = {
+
+    // Package.json
+    let pkgTemplate = {
       name: this.props.roleName,
       version: "1.0.0",
       main: "index.js",
-      // TODO this grab this dynamically
+      // TODO [GIT] grab this dynamically
       repository: "git@github.com:Aaron-K-T-Berry/generator-ansible.git",
       description: this.props.description,
       author: this.props.authorName,
@@ -169,30 +189,36 @@ module.exports = class extends Generator {
       scripts: {
         venv: "virtualenv venv",
         "venv-activate": "source venv/bin/activate",
-        requirements: "pip install -r requirements.txt",
-        check: "molecule check",
-        converge: "molecule converge",
-        create: "molecule create",
-        dependency: "molecule dependency",
-        destroy: "molecule destroy",
-        idempotence: "molecule idempotence",
-        lint: "molecule lint",
-        list: "molecule list",
-        login: "molecule login",
-        matrix: "molecule matrix",
-        prepare: "molecule prepare",
-        "side-effect": "molecule side-effect",
-        syntax: "molecule syntax",
-        test: "molecule test",
-        verify: "molecule verify"
+        requirements: "pip install -r requirements.txt"
       }
     };
-    this.fs.writeJSON(path.join(this.props.roleRoot, "package.json"), pkgTemplate);
+    if (this.props.includeMolecule) {
+      pkgTemplate = {
+        ...pkgTemplate,
+        ...{
+          check: "molecule check",
+          converge: "molecule converge",
+          create: "molecule create",
+          dependency: "molecule dependency",
+          destroy: "molecule destroy",
+          idempotence: "molecule idempotence",
+          lint: "molecule lint",
+          list: "molecule list",
+          login: "molecule login",
+          matrix: "molecule matrix",
+          prepare: "molecule prepare",
+          "side-effect": "molecule side-effect",
+          syntax: "molecule syntax",
+          test: "molecule test",
+          verify: "molecule verify"
+        }
+      };
+    }
+    this.fs.writeJSON(path.join(roleRoot, "package.json"), pkgTemplate);
   }
 
   install() {
     // Fixing permissions
     exec(`chmod +x ${path.join(this.destinationRoot(), this.props.roleRoot, "run-test.sh")}`);
-    // this.installDependencies({ bower: false });
   }
 };
