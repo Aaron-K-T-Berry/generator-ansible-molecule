a32b1ff0ef335a14ba38a976adcd24da4108ae0f
Aaron Berry <aaron.abm@gmail.com>
2019-03-06 16:33:52 +1100

    test(index.js): Increased test coverage for app/index.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,220 +3,188 @@ const helpers = require("yeoman-test");
 const assert = require("yeoman-assert");
 const fs = require("fs");
 
-const buildPkgJson = params => {
-  let template = {
-    name: params.roleName,
-    version: "1.0.0",
-    main: "index.js",
-    repository: params.gitIncludeRepoUrl ? params.gitRepoUrl : "",
-    description: params.description,
-    author: params.gitAuthorName,
-    license: params.license,
-    dependencies: {},
-    scripts: {
-      venv: "virtualenv venv",
-      "venv-activate": "source venv/bin/activate",
-      requirements: "pip install -r requirements.txt"
-    }
-  };
-
-  if (params.includeMolecule) {
-    template = {
-      ...template,
-      ...{
-        check: "molecule check",
-        converge: "molecule converge",
-        create: "molecule create",
-        dependency: "molecule dependency",
-        destroy: "molecule destroy",
-        idempotence: "molecule idempotence",
-        lint: "molecule lint",
-        list: "molecule list",
-        login: "molecule login",
-        matrix: "molecule matrix",
-        prepare: "molecule prepare",
-        "side-effect": "molecule side-effect",
-        syntax: "molecule syntax",
-        test: "molecule test",
-        verify: "molecule verify"
-      }
-    };
-  }
-  return template;
-};
+// TODO create more generic test helper functions
 
 const defaultPrompts = {
-  // BASIC
-  roleName: "test",
-  description: "Role description",
-  license: "MIT",
+	// BASIC
+	roleName: "test",
+	description: "Role description",
+	license: "MIT",
 
-  // GIT
-  gitAuthorName: "John smith",
-  gitAuthorEmail: "jsmith.email.com",
-  gitIncludeRepoUrl: false,
+	// GIT
+	gitAuthorName: "John smith",
+	gitAuthorEmail: "jsmith.email.com",
+	gitIncludeRepoUrl: false,
 
-  // MOLECULE
-  includeMolecule: false,
+	// MOLECULE
+	includeMolecule: false,
 
-  // META
-  includeMeta: false
+	// META
+	includeMeta: false
 };
 
-describe("Defaults included", () => {
-  const prompts = { ...defaultPrompts };
+const roleRoot = defaultPrompts.roleName;
 
-  beforeEach(() => {
-    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts(prompts);
-  });
+describe("index.js", () => {
+	describe("Generates a default project", () => {
+		beforeEach(() => {
+			return helpers
+				.run(path.join(__dirname, "../generators/app"))
+				.withPrompts(defaultPrompts);
+		});
 
-  it("Copied defaults/main.yml", () => {
-    assert.file(`${prompts.roleName}/defaults/main.yml`);
-  });
+		it("Should copy over all files", () => {
+			paths = [
+				path.join(roleRoot, ".yamllint"),
+				path.join(roleRoot, "defaults", "main.yml"),
+				path.join(roleRoot, "handlers", "main.yml"),
+				path.join(roleRoot, "tasks", "main.yml"),
+				path.join(roleRoot, "vars", "main.yml"),
+				path.join(roleRoot, "README.md"),
+				path.join(roleRoot, "requirements.txt"),
+				path.join(roleRoot, "run-test.sh")
+			];
 
-  it("Copied handlers/main.yml", () => {
-    assert.file(`${prompts.roleName}/handlers/main.yml`);
-  });
+			for (item of paths) {
+				assert.file(item);
+			}
+		});
 
-  it("Didn't copy meta/main.yml", () => {
-    assert.noFile(`${prompts.roleName}/meta/main.yml`);
-  });
+		it("Should have the correct content for each file", () => {
+			paths = [
+				{
+					path: path.join(roleRoot, "README.md"),
+					contains: [
+						defaultPrompts.roleName,
+						`Author: ${defaultPrompts.gitAuthorName}`,
+						`Email: ${defaultPrompts.gitAuthorEmail}`
+					]
+				},
+				{
+					// TODO check the permissions of this file if possible
+					path: path.join(roleRoot, "run-test.sh"),
+					contains: [defaultPrompts.roleName]
+				},
+				{
+					path: path.join(roleRoot, "package.json"),
+					contains: [
+						defaultPrompts.roleName,
+						`"repository": ""`,
+						defaultPrompts.description,
+						defaultPrompts.gitAuthorName,
+						"MIT"
+					]
+				}
+			];
 
-  it("Didn't copy molecule/default/tests files", () => {
-    assert.noFile(`${prompts.roleName}/molecule/default/tests/test_default.py`);
-    assert.noFile(`${prompts.roleName}/molecule/default/tests/test_default.pyc`);
-  });
+			for (item of paths) {
+				for (check of item.contains) {
+					assert.fileContent(item.path, check);
+				}
+			}
+		});
+	});
 
-  it("Didn't copy molecule/default files", () => {
-    assert.noFile(`${prompts.roleName}/molecule/default/Dockerfile.j2`);
-    assert.noFile(`${prompts.roleName}/molecule/default/INSTALL.rst`);
-    assert.noFile(`${prompts.roleName}/molecule/default/molecule.yml`);
-    assert.noFile(`${prompts.roleName}/molecule/default/playbook.yml`);
-  });
+	describe("Generates a project with a repository url", () => {
+		const prompts = {
+			...defaultPrompts,
+			gitIncludeRepoUrl: true,
+			gitRepoUrl: "some-repo.com"
+		};
 
-  it("Copied tasks/main.yml", () => {
-    assert.file(`${prompts.roleName}/tasks/main.yml`);
-  });
+		beforeEach(() => {
+			return helpers
+				.run(path.join(__dirname, "../generators/app"))
+				.withPrompts(prompts);
+		});
 
-  it("Copied vars/main.yml", () => {
-    assert.file(`${prompts.roleName}/vars/main.yml`);
-  });
+		it("Should add the correct line to the repository key", () => {
+			assert.fileContent(
+				path.join(roleRoot, "package.json"),
+				`"repository": "${prompts.gitRepoUrl}"`
+			);
+		});
+	});
 
-  it("Copied .yamllint", () => {
-    assert.file(`${prompts.roleName}/.yamllint`);
-  });
+	describe("Generates a project with a molecule", () => {
+		const prompts = {
+			...defaultPrompts,
+			includeMolecule: true,
+			includeCircleCi: true
+		};
 
-  it("Copied README.md", () => {
-    assert.file(`${prompts.roleName}/README.md`);
-  });
+		beforeEach(() => {
+			return helpers
+				.run(path.join(__dirname, "../generators/app"))
+				.withPrompts(prompts);
+		});
 
-  it("Contents of README.md", () => {
-    assert.fileContent(`${prompts.roleName}/README.md`, prompts.roleName);
-    assert.fileContent(`${prompts.roleName}/README.md`, `Author: ${prompts.gitAuthorName}`);
-    assert.fileContent(`${prompts.roleName}/README.md`, `Email: ${prompts.gitAuthorEmail}`);
-  });
+		it("Should include all molecule files", () => {
+			paths = [
+				path.join(roleRoot, ".circleci"),
+				path.join(roleRoot, "molecule", "default", "Dockerfile.j2"),
+				path.join(roleRoot, "molecule", "default", "INSTALL.RST"),
+				path.join(roleRoot, "molecule", "default", "molecule.yml"),
+				path.join(roleRoot, "molecule", "default", "playbook.yml"),
+				path.join(roleRoot, "molecule", "default", "tests", "test_default.pyc"),
+				path.join(roleRoot, "molecule", "default", "tests", "test_default.py")
+			];
 
-  it("Copied requirements.txt", () => {
-    assert.file(`${prompts.roleName}/requirements.txt`);
-  });
+			for (item of paths) {
+				assert.file(item);
+			}
+		});
 
-  it("Copied run-test.sh", () => {
-    assert.file(`${prompts.roleName}/run-test.sh`);
-  });
+		it("Should have the correct content for each molecule file", () => {
+			paths = [
+				{
+					path: path.join(roleRoot, "molecule", "default", "playbook.yml"),
+					contains: [`- role: ${defaultPrompts.roleName}`]
+				}
+			];
 
-  it("Contents of run-test.sh", () => {
-    assert.fileContent(
-      `${prompts.roleName}/run-test.sh`,
-      `echo "Running test for ${prompts.roleName}"`
-    );
-  });
+			for (item of paths) {
+				for (check of item.contains) {
+					assert.fileContent(item.path, check);
+				}
+			}
+		});
+	});
 
-  // TODO [TESTS] check exec permissions of this file
+	describe("Generates a project with a meta folder", () => {
+		prompts = { ...defaultPrompts, includeMeta: true, metaCompany: "company" };
+		beforeEach(() => {
+			return helpers
+				.run(path.join(__dirname, "../generators/app"))
+				.withPrompts(prompts);
+		});
 
-  it("Copied package.json", () => {
-    assert.file(`${prompts.roleName}/package.json`);
-  });
+		it("Should copy over all meta files", () => {
+			paths = [path.join(roleRoot, "meta", "main.yml")];
 
-  it("Content package.json", () => {
-    assert.JSONFileContent(`${prompts.roleName}/package.json`, buildPkgJson(prompts));
-  });
-});
+			for (item of paths) {
+				assert.file(item);
+			}
+		});
 
-describe("Git repo urls", () => {
-  describe("Include repo url", () => {
-    const prompts = { ...defaultPrompts, gitIncludeRepoUrl: true, gitRepoUrl: "repo.com" };
-    beforeEach(() => {
-      return helpers.run(path.join(__dirname, "../generators/app")).withPrompts(prompts);
-    });
+		it("Should have the correct content for each file", () => {
+			paths = [
+				{
+					path: path.join(roleRoot, "meta", "main.yml"),
+					contains: [
+						`author: ${prompts.gitAuthorName}`,
+						`description: ${prompts.description}`,
+						`company: ${prompts.metaCompany}`,
+						`license: ${prompts.license}`
+					]
+				}
+			];
 
-    it("Contents of package.json", () => {
-      assert.fileContent(`${prompts.roleName}/package.json`, prompts.gitRepoUrl);
-    });
-  });
-});
-
-describe("Molecule included", () => {
-  const prompts = { ...defaultPrompts, includeMolecule: true };
-
-  beforeEach(() => {
-    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts(prompts);
-  });
-
-  it("Copied molecule/default/tests files", () => {
-    assert.file(`${prompts.roleName}/molecule/default/tests/test_default.py`);
-    assert.file(`${prompts.roleName}/molecule/default/tests/test_default.pyc`);
-  });
-
-  it("Copied molecule/default files", () => {
-    assert.file(`${prompts.roleName}/molecule/default/Dockerfile.j2`);
-    assert.file(`${prompts.roleName}/molecule/default/INSTALL.rst`);
-    assert.file(`${prompts.roleName}/molecule/default/molecule.yml`);
-    assert.file(`${prompts.roleName}/molecule/default/playbook.yml`);
-  });
-
-  it("Contents of molecule/default/playbook.yml", () => {
-    assert.fileContent(
-      `${prompts.roleName}/molecule/default/playbook.yml`,
-      `- role: ${prompts.roleName}`
-    );
-  });
-
-  it("Content package.json", () => {
-    assert.JSONFileContent(`${prompts.roleName}/package.json`, buildPkgJson(prompts));
-  });
-});
-
-describe("CircleCi included", () => {
-  const prompts = { ...defaultPrompts, includeMolecule: true, includeCircleCi: true };
-
-  beforeEach(() => {
-    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts(prompts);
-  });
-
-  it("Copied .circleci/config.yml", () => {
-    assert.file(`${prompts.roleName}/.circleci/config.yml`);
-  });
-});
-
-describe("Meta included", () => {
-  const prompts = { ...defaultPrompts, ...{ includeMeta: true, metaCompany: "Company co." } };
-
-  beforeEach(() => {
-    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts(prompts);
-  });
-
-  it("Copied meta/main.yml", () => {
-    assert.file(`${prompts.roleName}/meta/main.yml`);
-  });
-
-  it("Contents meta/main.yml", () => {
-    assert.fileContent(`${prompts.roleName}/meta/main.yml`, `author: ${prompts.gitAuthorName}`);
-    assert.fileContent(`${prompts.roleName}/meta/main.yml`, `description: ${prompts.description}`);
-    assert.fileContent(`${prompts.roleName}/meta/main.yml`, `company: ${prompts.metaCompany}`);
-    assert.fileContent(`${prompts.roleName}/meta/main.yml`, `license: ${prompts.license}`);
-  });
-
-  it("Content package.json", () => {
-    assert.JSONFileContent(`${prompts.roleName}/package.json`, buildPkgJson(prompts));
-  });
+			for (item of paths) {
+				for (check of item.contains) {
+					assert.fileContent(item.path, check);
+				}
+			}
+		});
+	});
 });
