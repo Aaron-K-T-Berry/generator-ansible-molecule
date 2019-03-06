718bc18cf5b5c6a73140af46bfa3cddf9784b9f3
Aaron Berry <aaron.abm@gmail.com>
2019-03-06 15:00:20 +1100

    fix(package-builder): Fix package.json scripts
    Fixed molecule scripts not being added properly to the package.json

--- generators/app/src/package-builder.js
+++ generators/app/src/package-builder.js
@@ -1,49 +1,48 @@
 const getLicenseValue = require("./prompts").getLicenseValue;
 
-const buildPackageJSON = (props) => {
-  let pkgTemplate = {
-    name: props.roleName,
-    version: "1.0.0",
-    main: "index.js",
-    repository: props.gitIncludeRepoUrl ? props.gitRepoUrl : "",
-    description: props.description,
-    author: props.gitAuthorName,
-    license: getLicenseValue(props.license),
-    dependencies: {},
-    scripts: {
-      venv: "virtualenv venv",
-      "venv-activate": "source venv/bin/activate",
-      requirements: "pip install -r requirements.txt"
-    }
-  };
-  
-  if (props.includeMolecule) {
-    pkgTemplate = {
-      ...pkgTemplate,
-      ...{
-        check: "molecule check",
-        converge: "molecule converge",
-        create: "molecule create",
-        dependency: "molecule dependency",
-        destroy: "molecule destroy",
-        idempotence: "molecule idempotence",
-        lint: "molecule lint",
-        list: "molecule list",
-        login: "molecule login",
-        matrix: "molecule matrix",
-        prepare: "molecule prepare",
-        "side-effect": "molecule side-effect",
-        syntax: "molecule syntax",
-        test: "molecule test",
-        verify: "molecule verify"
-      }
-    };
-  }
+const buildPackageJSON = props => {
+	let pkgTemplate = {
+		name: props.roleName,
+		version: "1.0.0",
+		main: "index.js",
+		repository: props.gitIncludeRepoUrl ? props.gitRepoUrl : "",
+		description: props.description,
+		author: props.gitAuthorName,
+		license: getLicenseValue(props.license),
+		dependencies: {},
+		scripts: {
+			venv: "virtualenv venv",
+			"venv-activate": "source venv/bin/activate",
+			requirements: "pip install -r requirements.txt"
+		}
+	};
 
-  return pkgTemplate
-}
+	if (props.includeMolecule) {
+		pkgTemplate.scripts = {
+			...pkgTemplate.scripts,
+			...{
+				check: "molecule check",
+				converge: "molecule converge",
+				create: "molecule create",
+				dependency: "molecule dependency",
+				destroy: "molecule destroy",
+				idempotence: "molecule idempotence",
+				lint: "molecule lint",
+				list: "molecule list",
+				login: "molecule login",
+				matrix: "molecule matrix",
+				prepare: "molecule prepare",
+				"side-effect": "molecule side-effect",
+				syntax: "molecule syntax",
+				test: "molecule test",
+				verify: "molecule verify"
+			}
+		};
+	}
+
+	return pkgTemplate;
+};
 
 module.exports = {
-  buildPackageJSON
-}
-
+	buildPackageJSON
+};
