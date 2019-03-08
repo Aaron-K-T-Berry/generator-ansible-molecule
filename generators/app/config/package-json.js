b83bc822abdf6c66c56c609eabe6e61d6dee5e55
Aaron Berry <aaron.abm@gmail.com>
2019-03-09 03:05:14 +1100

    refactor: Moved config to their own folder and moved util functions to helper file

---
+++ generators/app/config/package-json.js
@@ -0,0 +1,21 @@
+const helper = require("../src/helper");
+
+const pkgTemplate = props => {
+	return {
+		name: props.roleName,
+		version: "1.0.0",
+		main: "index.js",
+		repository: props.gitIncludeRepoUrl ? props.gitRepoUrl : "",
+		description: props.description,
+		author: props.gitAuthorName,
+		license: helper.getLicenseValue(props.license),
+		dependencies: {},
+		scripts: {
+			venv: "virtualenv venv",
+			"venv-activate": "source venv/bin/activate",
+			requirements: "pip install -r requirements.txt"
+		}
+	};
+};
+
+module.exports = { pkgTemplate };
